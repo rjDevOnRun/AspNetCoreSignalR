1dea0e21745b4d7846cb75596a5dfd9ada19e2e8
RjDevOnrun <john.roby76@gmail.com>
2021-08-08 17:30:37 +0900

    Add project files.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Signal R with Asp.NET Core
+A simple chat app using SignalR in asp.net core
---
+++ WebAppSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebAppSignalR.Hubs
+{
+    // https://docs.microsoft.com/en-us/aspnet/core/tutorials/signalr?view=aspnetcore-5.0&tabs=visual-studio
+
+    public class ChatHub: Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
